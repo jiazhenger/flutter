2dbda9000ead775b8d33a269734866e8bb4a66e5
jiazer <464893998@qq.com>
2020-05-18 09:33:24 +0800

    0

--- app-react/src/common/config.js
+++
@@ -1,8 +0,0 @@
-/* ====================================== api 配置  ====================================== */
-const env = process.env.NODE_ENV === 'development';	// 是否为开发环境
-/* ====================================== 全局变量配置  ====================================== */
-module.exports = {
-	// 环境变量
-	env : env,
-	appUrl:'http://192.168.5.70:8020/jia-flutter/flutter_web_study/app/build/index.html/#/'
-}
--- app-react/src/pages/data-index.js
+++
@@ -1,8 +0,0 @@
-export default [
-	{ title: 'CSS 教程', path:'/css' },
-	{ title: 'JavaScriopt 教程', path:'/js' },
-	{ title: 'React 教程', path:'/react' },
-	{ title: 'Vue 教程', path:'/vue' },
-	{ title: 'Dart 教程', path:'/dart' },
-	{ title: 'Flutter 教程', path:'/flutter' },
-]
